060fa462fdfe37916a4f737c6cd55e19b6a08b89
enroute-LuisNavarrete <89099503+enroute-LuisNavarrete@users.noreply.github.com>
2021-08-27 02:40:48 -0500

    java prep

---
+++ java-prep-spr/src/test/java/com/springprep/javaprepspr/JavaPrepSprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprep.javaprepspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPrepSprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
